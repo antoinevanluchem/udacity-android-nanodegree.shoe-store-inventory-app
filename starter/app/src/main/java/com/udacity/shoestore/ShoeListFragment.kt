b256277b294e8963ca1a3cd2f2226dc518289a46
Antoine Van Luchem <antoine.van-luchem@bmw.de>
2024-05-22 09:48:29 +0200

    Dynamically fill up shoe list

--- starter/app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
@@ -5,13 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.FragmentShoeListBinding
+import com.udacity.shoestore.databinding.ShoeElementBinding
+import com.udacity.shoestore.models.Shoe
+
 
 class ShoeListFragment : Fragment() {
+
+    private lateinit var layoutInflater: LayoutInflater
+    private lateinit var binding: FragmentShoeListBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shoe_list, container, false)
+    ): View {
+        layoutInflater = inflater
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_list, container, false)
+
+        initShoeList(mutableListOf(Shoe("550", 43.0, "New Balance", "My favourite shoes")))
+
+        return binding.root
+    }
+
+    private fun initShoeList(shoeList: MutableList<Shoe>) {
+        for (shoe in shoeList) {
+            val inflatedShoeBinding: ShoeElementBinding = DataBindingUtil.inflate(
+                layoutInflater,
+                R.layout.shoe_element,
+                binding.shoeList,
+                false
+            )
+
+            inflatedShoeBinding.companyAndName.text = "${shoe.company} ${shoe.name}"
+            inflatedShoeBinding.size.text = shoe.size.toString()
+            inflatedShoeBinding.description.text = shoe.description
+            inflatedShoeBinding.shoeImage.setImageResource(R.drawable.new_balance_550_white)
+
+            binding.shoeList.addView(inflatedShoeBinding.root)
+        }
     }
 }
--- starter/app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
@@ -9,11 +9,6 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [WelcomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WelcomeFragment : Fragment() {
 
     override fun onCreateView(
