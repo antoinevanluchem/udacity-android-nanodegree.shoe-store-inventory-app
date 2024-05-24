cdf386fa39ed9a8f73baba48533a4e5013ce97a7
Antoine Van Luchem <antoine.van-luchem@bmw.de>
2024-05-24 14:15:48 +0200

    Set up supportActionBar

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,13 +2,27 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Timber.plant(Timber.DebugTree())
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        )
+        setSupportActionBar(binding.toolbar)
+        NavigationUI.setupActionBarWithNavController(this, findNavController(R.id.myNavHostFragment))
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.myNavHostFragment).navigateUp()
     }
 }
--- starter/app/src/main/java/com/udacity/shoestore/shoe/ShoeListFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/shoe/ShoeListFragment.kt
@@ -3,6 +3,8 @@ package com.udacity.shoestore.shoe
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
